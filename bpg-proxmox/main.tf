af2e6fb0e8a06cd69b104752c1ec9a35f45462ce
Rauland Jerliu <rauland.jerliu@gmail.com>
2025-09-29 07:40:44 +1000

    move blocks into own .tf

--- bpg-proxmox/main.tf
+++ bpg-proxmox/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.74.1"
-    }
-  }
-}
-
 provider "proxmox" {
   endpoint  = var.virtual_environment_endpoint
   api_token = var.virtual_environment_api_token
