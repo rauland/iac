cc132d8da98e275fb517728b2c92d3271e2fbb72
Rauland Jerliu <rauland.jerliu@gmail.com>
2025-10-11 17:06:33 +1100

    remove debug

--- proxmox/outputs.tf
+++ proxmox/outputs.tf
@@ -11,18 +11,13 @@ output "installer_image" {
 }
 
 output "talos_controlpane_configuration" {
-  value = module.cluster.talos_controlpane_configuration
+  value     = module.cluster.talos_controlpane_configuration
   sensitive = true
 }
 
 output "talos_worker_configuration" {
-  value = module.cluster.talos_worker_configuration
+  value     = module.cluster.talos_worker_configuration
   sensitive = true
-}
-
-resource "local_file" "worker_configuration" {
-  filename = "debug.txt"
-  content  = module.cluster.talos_worker_configuration
 }
 
 resource "local_file" "talosconfig" {
