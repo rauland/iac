d937747543f9c252148f7a7edbcdadd751bf01bc
Rauland Jerliu <rauland.jerliu@gmail.com>
2026-03-26 21:30:41 +1100

    Remove unused output for Proxmox virtual environment nodes

--- terraform/modules/cloud-image/outputs.tf
+++ terraform/modules/cloud-image/outputs.tf
@@ -2,11 +2,3 @@ output "id" {
   description = "ID of the downloaded cloud image"
   value       = proxmox_virtual_environment_download_file.cloud_image.id
 }
-
-output "data_proxmox_virtual_environment_nodes" {
-  value = {
-    names     = data.proxmox_virtual_environment_nodes.example.names
-    cpu_count = data.proxmox_virtual_environment_nodes.example.cpu_count
-    online    = data.proxmox_virtual_environment_nodes.example.online
-  }
-}
