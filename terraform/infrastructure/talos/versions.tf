5ec3b166a39996af9066d7326162cc288194a5b1
Rauland Jerliu <rauland.jerliu@gmail.com>
2026-03-27 21:56:02 +1100

    testing new provider version

--- terraform/infrastructure/talos/versions.tf
+++ terraform/infrastructure/talos/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.84.0"
+      version = "0.98.1"
     }
     talos = {
       source  = "siderolabs/talos"
